014ad373196f957f83d7686853dd808c6324e419
IgnacioTraverso <ignaciotraversobaturone@gmail.com>
2020-02-13 10:18:03 +0100

    commit 3

--- proyecto_psp/settings.py
+++ proyecto_psp/settings.py
@@ -121,6 +121,10 @@ LOGOUT_REDIRECT_URL = reverse_lazy('login')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_URL = '/static/'
+STATIC_ROOT = '/static'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'proyecto_psp/media')
 
